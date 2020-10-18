c26f9e4142f1578985f1d53fb6daa797e791c00e
Wesley <wesley.contato@hotmail.com>
2020-10-18 10:56:54 -0300

    Incluindo plugins

--- produtos/src/main/java/br/com/alura/maven/Produto.java
+++ produtos/src/main/java/br/com/alura/maven/Produto.java
@@ -4,10 +4,19 @@ public class Produto {
 
 	private final String nome;
 	private final double preco;
+	private final String categoria = "comida";
 
 	public Produto(String nome, double preco) {
 		super();
 		this.nome = nome;
 		this.preco = preco;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public double getPreco() {
+		return preco;
 	}
 }
